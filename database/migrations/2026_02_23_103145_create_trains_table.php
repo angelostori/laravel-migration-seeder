c96307785e794d1b2a3374740866b5372d79dc29
angelostori <angelostori13@gmail.com>
2026-02-23 12:05:46 +0100

    create a new migration file and define its schema

---
+++ database/migrations/2026_02_23_103145_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', length: 50);
+            $table->string('landing_station', length: 50);
+            $table->string('arrival_station', length: 50);
+            $table->time('landind_time', precision: 0);
+            $table->time('arrival_time', precision: 0);
+            $table->string('code_comp', length: 2);
+            $table->mediumInteger('code_train'); //5 numeri
+            $table->tinyInteger('carriages');
+            $table->boolean('is_on_time');
+            $table->boolean('is_aborted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
